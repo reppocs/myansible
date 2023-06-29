0b7cb1eda217137f7888a5691fc5033df40e3d5e
Corey Reppond <reppocs@gmail.com>
2023-06-28 21:38:26 -0400

    made it only compatible with ubuntu 22.04

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This is a script to set up the environment for running the playbook.
+#
+# It only supports Ubuntu 22.04.
+
 set -e
 
 DISTRO_NAME="$(grep '^ID=' /etc/os-release | awk -F= '{print $2}')"
@@ -10,38 +14,21 @@ err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: ERROR: $*" >&2 && exit 1
 }
 
-run_fedora () {
-    sudo dnf -y install ansible git curl -q
-}
-
-run_ubuntu() {
-    sudo apt-add-repository -y ppa:ansible/ansible > /dev/null
-    DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq
-    sudo apt-get -y install curl git software-properties-common ansible ansible-lint -qq
-    sudo apt-get -y autoremove -qq
-}
-
-git_clone() {
-    # clone the repo to the Downloads directory
-    [[ ! -d "$HOME/Downloads" ]] && err "$HOME/Downloads does not exist."
-    git clone https://github.com/reppocs/myansible.git "$HOME/Downloads"
-
-    # if the cloned repo directory exists, exit successfully
-    [[ -d "$HOME/Downloads/myansible" ]] && exit 0
-}
-
-# if the distro and version are supported, run the things
-if [[ "$DISTRO_NAME" == "ubuntu" ]]; then
-    if [[ "$DISTRO_VERSION" == "22.04" ]]; then
-        run_ubuntu
-        git_clone
+if [[ ! "$DISTRO_NAME" == "ubuntu" ]]; then
+    err "This script only supports Ubuntu 22.04."
+    if [[ ! "$DISTRO_VERSION" == "22.04" ]]; then
+        err "This script only supports Ubuntu 22.04." 
     fi
-elif [[ "$DISTRO_NAME" == "fedora" ]]; then
-    if [[ "$DISTRO_VERSION" == "38" ]]; then
-        run_fedora
-        git_clone
-    fi 
 fi
+                                                                                                                                                            
+sudo apt-add-repository -y ppa:ansible/ansible > /dev/null
+DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq
+sudo apt-get -y install curl git software-properties-common ansible ansible-lint -qq
+sudo apt-get -y autoremove -qq
 
-# if none of those matched, error out
-err "Distribution and/or version unsupported."
+# clone the repo to the Downloads directory
+[[ ! -d "$HOME/Downloads" ]] && err "$HOME/Downloads does not exist."
+git clone https://github.com/reppocs/myansible.git "$HOME/Downloads"
+
+# if the cloned repo directory exists, exit successfully
+[[ -d "$HOME/Downloads/myansible" ]] && exit 0
