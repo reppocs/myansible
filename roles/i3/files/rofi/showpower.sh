92600b59f32c2787cc257e0f21c35c2a877a3057
Corey Reppond <reppocs@gmail.com>
2023-07-26 16:00:24 -0700

    changed back to simple configs from theme switcher thing

---
+++ roles/i3/files/rofi/showpower.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#THEME="~/.config/rofi/themes/gruvbox.rasi"
+THEME="~/.config/rofi/themes/onedark.rasi"
+
+rofi -show power-menu -modi power-menu:'~/.config/rofi/rofi-power-menu --no-symbols' -font "JetBrainsMono Nerd Font Medium 10" -theme "$THEME" -theme-str 'window {width: 20em;} listview {lines: 6;}'
