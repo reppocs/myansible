406aa124737b15cf4859066de6f197ab2755c328
Corey Reppond <reppocs@gmail.com>
2026-05-11 17:25:45 -0400

    changed comment in init.sh to reflect os version change

--- init.sh
+++ init.sh
@@ -2,14 +2,14 @@
 
 # This is a script to set up the environment for running the playbook.
 #
-# Currently, it only supports Ubuntu (22.04 / 24.04), and Fedora 42 / 43.
+# Currently, it only supports Ubuntu (24.04 / 26.04), and Fedora 43 / 44.
 
 DISTRO_NAME="$(grep '^ID=' /etc/os-release | awk -F= '{print $2}')"
-DISTRO_VERSION="$(grep '^VERSION_ID' /etc/os-release | awk -F= '{print $2}' |sed -e 's/\"//g')"
+DISTRO_VERSION="$(grep '^VERSION_ID' /etc/os-release | awk -F= '{print $2}' | sed -e 's/\"//g')"
 
 # handle errors in a uniform manner
 err() {
-    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: ERROR: $*" >&2 && exit 1
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: ERROR: $*" >&2 && exit 1
 }
 
 fedora_things() {
@@ -17,7 +17,7 @@ fedora_things() {
 }
 
 ubuntu_things() {
-  sudo apt-add-repository -y ppa:ansible/ansible > /dev/null
+  sudo apt-add-repository -y ppa:ansible/ansible >/dev/null
   DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq
   sudo apt-get -y install curl git software-properties-common ansible ansible-lint -qq
 }
@@ -28,8 +28,7 @@ git_things() {
   git -C "$HOME/Downloads" clone https://github.com/reppocs/myansible.git
 
   # if the cloned repo directory exists, exit successfully
-  if [[ -d "$HOME/Downloads/myansible" ]]
-  then
+  if [[ -d "$HOME/Downloads/myansible" ]]; then
     exit 0
   else
     err "The repo was not cloned successfully"
@@ -38,27 +37,25 @@ git_things() {
 
 # if it's supported, do the thing
 case $DISTRO_NAME in
-  fedora)
-    if [[ $DISTRO_VERSION == 43 || $DISTRO_VERSION == 44 ]]
-    then
-      fedora_things
-      git_things
-    else
-      err "This is an unsupported version of $DISTRO_NAME"
-    fi
-    ;;
+fedora)
+  if [[ $DISTRO_VERSION == 43 || $DISTRO_VERSION == 44 ]]; then
+    fedora_things
+    git_things
+  else
+    err "This is an unsupported version of $DISTRO_NAME"
+  fi
+  ;;
 
-  ubuntu)
-    if [[ $DISTRO_VERSION == "24.04" || $DISTRO_VERSION == "26.04" ]]
-    then
-      ubuntu_things
-      git_things
-    else
-      err "This is an unsupported version of $DISTRO_NAME"
-    fi
-    ;;
+ubuntu)
+  if [[ $DISTRO_VERSION == "24.04" || $DISTRO_VERSION == "26.04" ]]; then
+    ubuntu_things
+    git_things
+  else
+    err "This is an unsupported version of $DISTRO_NAME"
+  fi
+  ;;
 
-  *)
-    err "This is an unsupported distribution"
-    ;;
+*)
+  err "This is an unsupported distribution"
+  ;;
 esac
